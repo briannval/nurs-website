3f7a97f45ecacc2b8e133f2f1c5c750687e03a4b
briannval <brianvalentinoadhitya@gmail.com>
2024-01-26 17:49:40 -0800

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,173 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import Head from "next/head";
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Icon,
+  useColorModeValue,
+  createIcon,
+  Flex,
+  SimpleGrid,
+  Image,
+  StackDivider,
+} from "@chakra-ui/react";
+
+import {
+  IoAnalyticsSharp,
+  IoLogoBitcoin,
+  IoSearchSharp,
+} from "react-icons/io5";
+import { ReactElement } from "react";
+
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
+
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "3xl", sm: "5xl", md: "8xl" }}
+            lineHeight={"110%"}
           >
-            By{" "}
+            Are you
+            <Text as={"span"} color={"orange.400"}>
+              {" "}
+              bored?
+            </Text>
+          </Heading>
+          <Text
+            color={"gray.500"}
+            fontSize={{ base: "xs", sm: "md", md: "xl" }}
+            as="b"
+          >
+            Don't worry, we've got you a place to slack!
+          </Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          >
+            <Button
+              colorScheme={"orange"}
+              bg={"orange.400"}
+              rounded={"full"}
+              px={6}
+              _hover={{
+                bg: "white.500",
+              }}
+            >
+              Get Started
+            </Button>
+            <Button variant={"link"} mt={4} colorScheme={"black"} size={"sm"}>
+              Leave a review
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+      <Container maxW={"5xl"} py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={20}>
+          <Stack spacing={4}>
+            <Text
+              textTransform={"uppercase"}
+              color={"blue.400"}
+              fontWeight={600}
+              fontSize={"sm"}
+              bg={useColorModeValue("blue.50", "blue.900")}
+              p={1}
+              alignSelf={"flex-start"}
+              rounded={"md"}
+            >
+              BETA
+            </Text>
+            <Heading>Try out some activities!</Heading>
+            <Text color={"gray.500"} fontSize={"lg"}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore
+            </Text>
+            <Stack
+              spacing={4}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
+              }
+            >
+              <Feature
+                icon={
+                  <Icon
+                    as={IoAnalyticsSharp}
+                    color={"yellow.500"}
+                    w={5}
+                    h={5}
+                  />
+                }
+                iconBg={useColorModeValue("yellow.100", "yellow.900")}
+                text={"Random Quote Generator"}
+              />
+              <Feature
+                icon={
+                  <Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text={"Random Joke Generator"}
+              />
+              <Feature
+                icon={
+                  <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("purple.100", "purple.900")}
+                text={"Random Image Generator"}
+              />
+            </Stack>
+          </Stack>
+          <Flex>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              rounded={"md"}
+              alt={"feature image"}
+              src={
+                "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              }
+              objectFit={"cover"}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </>
   );
 }
