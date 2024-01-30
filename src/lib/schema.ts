26cc62c2997c5b4e1263ddcf523d83e27f9b4f4c
briannval <brianvalentinoadhitya@gmail.com>
2024-01-30 14:35:50 -0800

    Added slight db instructions

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,5 +1,16 @@
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
+/* 
+To update the schema, either:
+1. MIGRATE
+npx drizzle-kit generate:pg -> npx tsx scripts/migrate.ts
+YES migration files
+
+2. PUSH
+npx drizzle-kit push:pg
+NO migration files
+*/
+
 export const reviews = pgTable("reviews", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
